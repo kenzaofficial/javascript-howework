5c1daf8b5fefccd0d6b6126eff8840af56999a9d
Юрий Стецкий <yuraha15@yandex.ru>
2022-11-08 12:47:21 +0200

    task 1 is rdy90%

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,28 @@
-console.log('123');
+'use strict';
+
+let money = prompt('Ваш бюджет на месяц?');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+let typeOfPaymentOne = prompt(
+  'Введите обязательную статью расходов в этом месяце'
+);
+let amountofPaymentOne = prompt('Во сколько обойдется?');
+let typeOfPaymentTwo = prompt(
+  'Введите обязательную статью расходов в этом месяце'
+);
+let amountofPaymentTwo = prompt('Во сколько обойдется?');
+
+var appData = {
+  budget: money,
+  timeData: time,
+  expenses: {
+    typeOfPaymentOne: amountofPaymentOne,
+    typeOfPaymentTwo: amountofPaymentTwo,
+  },
+  optionalExpenses: {},
+  income: [],
+  savings: false,
+};
+
+console.log(appData);
+
+console.log(appData.budget / 30);
