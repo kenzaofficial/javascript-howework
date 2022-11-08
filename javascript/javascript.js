bce22b60959c534d57ac64be466582999859c35b
Юрий Стецкий <yuraha15@yandex.ru>
2022-11-08 21:25:31 +0200

    new task

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,28 +1,70 @@
 'use strict';
 
-let money = prompt('Ваш бюджет на месяц?');
+let money = +prompt('Ваш бюджет на месяц?');
 let time = prompt('Введите дату в формате YYYY-MM-DD');
-let typeOfPaymentOne = prompt(
-  'Введите обязательную статью расходов в этом месяце'
-);
-let amountofPaymentOne = prompt('Во сколько обойдется?');
-let typeOfPaymentTwo = prompt(
-  'Введите обязательную статью расходов в этом месяце'
-);
-let amountofPaymentTwo = prompt('Во сколько обойдется?');
 
 var appData = {
   budget: money,
   timeData: time,
   expenses: {
-    typeOfPaymentOne: amountofPaymentOne,
-    typeOfPaymentTwo: amountofPaymentTwo,
+    // typeOfPayment: amountofPayment,
   },
   optionalExpenses: {},
   income: [],
   savings: false,
 };
 
-console.log(appData);
+for (let i = 0; i < 2; i++) {
+  let typeOfPayment = prompt(
+      'Введите обязательную статью расходов в этом месяце',
+      ''
+    ),
+    amountOfPayment = prompt('Во сколько обойдется?', '');
+  if (
+    typeof typeOfPayment === 'string' &&
+    typeof (typeOfPayment != null) &&
+    amountOfPayment != null &&
+    typeOfPayment != '' &&
+    amountOfPayment != '' &&
+    typeOfPayment.length < 50
+  ) {
+    console.log('done');
+    appData.expenses[typeOfPayment] = amountOfPayment;
+  } else {
+    console.log('Необходимо заполнить поля данных!');
+  }
+}
 
-console.log(appData.budget / 30);
+// альтернативній вариант
+
+// let i = 0;
+// while (i < 2) {
+//   let typeOfPayment = prompt(
+//       'Введите обязательную статью расходов в этом месяце',
+//       ''
+//     ),
+//     amountOfPayment = prompt('Во сколько обойдется?', '');
+//   if (
+//     typeof typeOfPayment === 'string' &&
+//     typeof (typeOfPayment != null) &&
+//     amountOfPayment != null &&
+//     typeOfPayment != '' &&
+//     amountOfPayment != '' &&
+//     typeOfPayment.length < 50
+//   ) {
+//     console.log('done');
+//     appData.expenses[typeOfPayment] = amountOfPayment;
+//   } else {
+//     console.log('Необходимо заполнить поля данных!');
+//   }
+//   i++;
+// }
+
+appData.moneyPerDay = appData.budget / 30;
+console.log('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+  console.log('Вы нищеброд');
+} else {
+  console.log('Вы более менее');
+}
