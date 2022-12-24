80fc66165fd05736c6e2e6b5d6463029b2b5797b
Юрий Стецкий <yuraha15@yandex.ru>
2022-12-24 23:27:08 +0200

    task is rdy

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,70 +1,88 @@
 'use strict';
 
-let money = +prompt('Ваш бюджет на месяц?');
-let time = prompt('Введите дату в формате YYYY-MM-DD');
+let money;
+let time;
+let appData;
+function start() {
+  money = +prompt('Ваш бюджет на месяц?');
+  time = prompt('Введите дату в формате YYYY-MM-DD');
 
-var appData = {
-  budget: money,
-  timeData: time,
-  expenses: {
-    // typeOfPayment: amountofPayment,
-  },
-  optionalExpenses: {},
-  income: [],
-  savings: false,
-};
+  appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: true,
+    chooseExpenses: function () {
+      for (let i = 0; i < 2; i++) {
+        let typeOfPayment = prompt(
+            'Введите обязательную статью расходов в этом месяце',
+            ''
+          ),
+          amountOfPayment = prompt('Во сколько обойдется?', '');
+        if (
+          typeof typeOfPayment === 'string' &&
+          typeof (typeOfPayment != null) &&
+          amountOfPayment != null &&
+          typeOfPayment != '' &&
+          amountOfPayment != '' &&
+          typeOfPayment.length < 50
+        ) {
+          console.log('Поля заполнены правильно');
+          appData.expenses[typeOfPayment] = amountOfPayment;
+        } else {
+          console.log('Необходимо заполнить поля данных!');
+        }
+      }
+    },
+  };
+}
+start();
 
-for (let i = 0; i < 2; i++) {
-  let typeOfPayment = prompt(
-      'Введите обязательную статью расходов в этом месяце',
-      ''
-    ),
-    amountOfPayment = prompt('Во сколько обойдется?', '');
-  if (
-    typeof typeOfPayment === 'string' &&
-    typeof (typeOfPayment != null) &&
-    amountOfPayment != null &&
-    typeOfPayment != '' &&
-    amountOfPayment != '' &&
-    typeOfPayment.length < 50
-  ) {
-    console.log('done');
-    appData.expenses[typeOfPayment] = amountOfPayment;
+function detectDayBudget() {
+  appData.moneyPerDay = (appData.budget / 30).toFixed(1);
+  alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+}
+detectDayBudget();
+
+function detectLevel() {
+  if (appData.moneyPerDay < 100) {
+    console.log('Вы нищеброд');
   } else {
-    console.log('Необходимо заполнить поля данных!');
+    console.log('Вы более менее');
   }
 }
+detectLevel();
 
-// альтернативній вариант
-
-// let i = 0;
-// while (i < 2) {
-//   let typeOfPayment = prompt(
-//       'Введите обязательную статью расходов в этом месяце',
-//       ''
-//     ),
-//     amountOfPayment = prompt('Во сколько обойдется?', '');
-//   if (
-//     typeof typeOfPayment === 'string' &&
-//     typeof (typeOfPayment != null) &&
-//     amountOfPayment != null &&
-//     typeOfPayment != '' &&
-//     amountOfPayment != '' &&
-//     typeOfPayment.length < 50
-//   ) {
-//     console.log('done');
-//     appData.expenses[typeOfPayment] = amountOfPayment;
-//   } else {
-//     console.log('Необходимо заполнить поля данных!');
-//   }
-//   i++;
-// }
-
-appData.moneyPerDay = appData.budget / 30;
-console.log('Ежедневный бюджет: ' + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-  console.log('Вы нищеброд');
-} else {
-  console.log('Вы более менее');
+function checkSavings() {
+  if (appData.savings == true) {
+    let save = +prompt('Какова сумма накоплений?'),
+      percent = +prompt('Под какой процент?');
+    appData.monthIncome = (save / 100 / 12) * percent;
+    appData.income.unshift(appData.monthIncome);
+    alert('Доход в месяц с вашего депозита ' + appData.monthIncome);
+  }
 }
+checkSavings();
+
+function chooseOptExpenses() {
+  for (let i = 0; i < 3; i++) {
+    let typeOfOptExpense = prompt('Статья необязательных расходов?');
+    let amountOfOptExpense = +prompt('Сколько потратили на эту хрень?');
+    if (
+      typeof typeOfOptExpense === 'string' &&
+      typeof (typeOfOptExpense != null) &&
+      amountOfOptExpense != null &&
+      typeOfOptExpense != '' &&
+      amountOfOptExpense != '' &&
+      typeOfOptExpense.length < 50
+    ) {
+      appData.optionalExpenses[typeOfOptExpense] = amountOfOptExpense;
+    } else {
+      console.log('Необходимо заполнить поля данных верным способом!');
+    }
+  }
+}
+chooseOptExpenses();
+console.log(appData);
