7cbf4bbae8410b9b652b527b2579b4fa9b326222
Юрий Стецкий <yuraha15@yandex.ru>
2022-12-27 23:00:17 +0200

    taks 5 is ready

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -36,53 +36,86 @@ function start() {
         }
       }
     },
+
+    detectDayBudget: function () {
+      appData.moneyPerDay = (appData.budget / 30).toFixed(1);
+      alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+    },
+    detectLevel: function () {
+      if (appData.moneyPerDay < 100) {
+        console.log('Вы нищеброд');
+      } else {
+        console.log('Вы более менее');
+      }
+    },
+    checkSavings: function () {
+      if (appData.savings == true) {
+        let save = +prompt('Какова сумма накоплений?'),
+          percent = +prompt('Под какой процент?');
+        appData.monthIncome = (save / 100 / 12) * percent;
+        appData.income.unshift(appData.monthIncome);
+        alert('Доход в месяц с вашего депозита ' + appData.monthIncome);
+      }
+    },
+    chooseOptExpenses: function () {
+      for (let i = 0; i < 3; i++) {
+        let typeOfOptExpense = prompt('Статья необязательных расходов?');
+        let amountOfOptExpense = +prompt('Сколько потратили на эту хрень?');
+        if (
+          typeof typeOfOptExpense === 'string' &&
+          typeof (typeOfOptExpense != null) &&
+          amountOfOptExpense != null &&
+          typeOfOptExpense != '' &&
+          amountOfOptExpense != '' &&
+          typeOfOptExpense.length < 50
+        ) {
+          appData.optionalExpenses[typeOfOptExpense] = amountOfOptExpense;
+        } else {
+          console.log('Необходимо заполнить поля данных верным способом!');
+        }
+      }
+    },
+    chooseIncome: function () {
+      let items = prompt(
+        'Что принесет доп. доход??(Перечислите через запятую)'
+      );
+
+      if (items != '' && items != null) {
+        console.log('введена строка, она как минимум не пустая, отлично');
+        appData.income = items.split(',');
+        appData.income.push(prompt('может еще что то забыли добавить?'));
+        appData.income.sort();
+        appData.income.forEach(function (item, j) {
+          alert(`Способы дополнительного заработка: ${j + 1}. ${item}`);
+        });
+      } else {
+        console.log('либо єто не строка либо она пустая либо вы нажали отмена');
+      }
+    },
   };
 }
 start();
-
-function detectDayBudget() {
-  appData.moneyPerDay = (appData.budget / 30).toFixed(1);
-  alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+for (let key in appData) {
+  console.log(`Наша программа включает в себя данные: ${key}`);
 }
-detectDayBudget();
 
-function detectLevel() {
-  if (appData.moneyPerDay < 100) {
-    console.log('Вы нищеброд');
-  } else {
-    console.log('Вы более менее');
-  }
-}
-detectLevel();
+// 1) Написать проверку, что пользователь может:
 
-function checkSavings() {
-  if (appData.savings == true) {
-    let save = +prompt('Какова сумма накоплений?'),
-      percent = +prompt('Под какой процент?');
-    appData.monthIncome = (save / 100 / 12) * percent;
-    appData.income.unshift(appData.monthIncome);
-    alert('Доход в месяц с вашего депозита ' + appData.monthIncome);
-  }
-}
-checkSavings();
+// ·        Ввести в дополнительных доходах (chooseIncome) только строку
 
-function chooseOptExpenses() {
-  for (let i = 0; i < 3; i++) {
-    let typeOfOptExpense = prompt('Статья необязательных расходов?');
-    let amountOfOptExpense = +prompt('Сколько потратили на эту хрень?');
-    if (
-      typeof typeOfOptExpense === 'string' &&
-      typeof (typeOfOptExpense != null) &&
-      amountOfOptExpense != null &&
-      typeOfOptExpense != '' &&
-      amountOfOptExpense != '' &&
-      typeOfOptExpense.length < 50
-    ) {
-      appData.optionalExpenses[typeOfOptExpense] = amountOfOptExpense;
-    } else {
-      console.log('Необходимо заполнить поля данных верным способом!');
-    }
-  }
-}
-chooseOptExpenses();
-console.log(appData);
+// ·        Не может оставить строку пустой
+
+// ·        Не может отменить вопрос
+
+// 2) При помощи метода перебора массива(forEach) вывести на экран сообщение "Способы доп. заработка: " и полученные способы (внутри chooseIncome)
+
+// ·        Товары должны начинаться с 1, а не с 0. Выполняем этот пункт в chooseIncome.
+
+// 3) Используя цикл for in для объекта (appData) вывести в консоль сообщение "Наша программа включает в себя данные: " (вывести весь appData)
+
+// 4) Проверить, чтобы все работало и не было ошибок в консоли
+
+// 5) Добавить папку с уроком на свой GitHub
+// Вопросы к этому заданию
+
+// Как перебрать свойства объекта?
