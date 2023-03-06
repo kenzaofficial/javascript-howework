6106b858c5311f30bd66e666dfaa80fc8fe4eaad
Юрий Стецкий <yuraha15@yandex.ru>
2023-03-06 13:00:36 +0200

    slider task added

--- lesson-submit/js/script.js
+++ lesson-submit/js/script.js
@@ -190,6 +190,57 @@ window.addEventListener("DOMContentLoaded", function () {
   //   event.preventDefault();
   // });
 
+  // ----------------------slider------------------------------------
+
+  let slideIndex = 1; /* параметр текущего слайда */
+  let slides = document.querySelectorAll(".slider-item");
+  let prev = document.querySelector(".prev");
+  let next = document.querySelector(".next");
+  let dotsWrap = document.querySelector(".slider-dots");
+  let dots = document.querySelectorAll(".dot");
+
+  showSlides(slideIndex);
+
+  function showSlides(currentSlide) {
+    if (currentSlide > slides.length) {
+      slideIndex = 1;
+    }
+    if (currentSlide < 1) {
+      slideIndex = slides.length;
+    }
+    slides.forEach((item) => (item.style.display = "none"));
+    dots.forEach((item) => item.classList.remove("dot-active"));
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("dot-active");
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
+
+  prev.addEventListener("click", function () {
+    plusSlides(-1);
+  });
+
+  next.addEventListener("click", function () {
+    plusSlides(1);
+  });
+
+  dotsWrap.addEventListener("click", function (event) {
+    for (let i = 0; i < dots.length + 1; i++) {
+      if (
+        event.target.classList.contains("dot") &&
+        event.target == dots[i - 1]
+      ) {
+        currentSlide(i);
+      }
+    }
+  });
+
   // --------------------------calculator--------------------
 
   let persons = document.querySelectorAll(".counter-block-input")[0],
