35abf1223ce033f33cf6946ff4dd08d4ad0c6bad
Юрий Стецкий <yuraha15@yandex.ru>
2023-03-06 09:41:12 +0200

    calculator task

--- lesson-submit/js/script.js
+++ lesson-submit/js/script.js
@@ -150,26 +150,8 @@ window.addEventListener("DOMContentLoaded", function () {
 
   setClock("timer", deadline);
 
-  // let message = {
-  //   loading: "Загрузка...",
-  //   success: "спасибо! скоро с вами свяжимся",
-  //   error: "что то пошшло не так",
-  // };
-
-  // let form = document.querySelector(".main-form");
-  // let input = form.getElementsByTagName("input");
-  // let statusMessage = document.createElement("div");
-
-  // statusMessage.classList.add("status");
-  // form.addEventListener("submit", function (event) {
-  //   event.preventDefault();
-  // });
-
-  // pop-up--------------------------------------
-
   const modalBtn = document.querySelector(".more");
   const overlay = document.querySelector(".overlay");
-  const popUp = document.querySelector(".popup");
   const popUpCloseBtn = document.querySelector(".popup-close");
   const body = document.querySelector("body");
 
@@ -190,6 +172,63 @@ window.addEventListener("DOMContentLoaded", function () {
     if (event.target === overlay) {
       popUpclose();
       body.style.overflow = "auto";
+    }
+  });
+
+  // let message = {
+  //   loading: "Загрузка...",
+  //   success: "спасибо! скоро с вами свяжимся",
+  //   error: "что то пошшло не так",
+  // };
+
+  // let form = document.querySelector(".main-form");
+  // let input = form.getElementsByTagName("input");
+  // let statusMessage = document.createElement("div");
+
+  // statusMessage.classList.add("status");
+  // form.addEventListener("submit", function (event) {
+  //   event.preventDefault();
+  // });
+
+  // --------------------------calculator--------------------
+
+  let persons = document.querySelectorAll(".counter-block-input")[0],
+    restDays = document.querySelectorAll(".counter-block-input")[1],
+    place = document.getElementById("select"),
+    totalValue = document.getElementById("total"),
+    personsSum = 0,
+    daysSum = 0,
+    total = 0;
+
+  totalValue.innerHTML = 0;
+
+  persons.addEventListener("change", function () {
+    personsSum = +this.value;
+    total = (daysSum + personsSum) * 4000;
+
+    if (restDays.value == "") {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total;
+    }
+  });
+
+  restDays.addEventListener("change", function () {
+    daysSum = +this.value;
+    total = (daysSum + personsSum) * 4000;
+    if (persons.value == "") {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total;
+    }
+  });
+
+  place.addEventListener("change", function () {
+    if (restDays.value == "" || persons.value == "") {
+      totalValue.innerHTML = 0;
+    } else {
+      let a = total;
+      totalValue.innerHTML = a * this.options[this.selectedIndex].value;
     }
   });
 });
